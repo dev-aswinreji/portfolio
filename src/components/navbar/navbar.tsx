55bb9f2b047bcb0e336feff50c24201eb3ae15e7
Aswin Reji <aswinreji.004@gmail.com>
2024-12-11 20:50:48 +0000

    feat: Added navbar for my portfolio

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar (){
+    return (
+        <nav className="navbar">
+            <div className="nav-left">Aswin Reji</div>
+            <div className="nav-right">
+                <span>About</span>
+                <span>Contact</span>
+            </div>
+        </nav>
+    )
+}

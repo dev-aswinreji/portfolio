8830094358741f2e187657277cc8d450af604a56
Aswin Reji <aswinreji.004@gmail.com>
2024-12-11 21:14:16 +0000

    feat: Added github icon and about contact name linkedin icon and hyperlink to navbar section

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,19 @@
-export default function Navbar (){
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import "./navbar.css"
+
+export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="nav-left">Aswin Reji</div>
             <div className="nav-right">
                 <span>About</span>
                 <span>Contact</span>
+                <a href="https://github.com/dev-aswinreji" target="_blank" className="nav-icon">
+                    <FaGithub />
+                </a>
+                <a href="https://linkedin.com/in/dev-aswinreji" target="_blank" className="nav-icon">
+                    <FaLinkedin />
+                </a>
             </div>
         </nav>
     )

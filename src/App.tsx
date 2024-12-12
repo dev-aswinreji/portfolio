5b1a12480b8b7b38a57c9f7dc4070cce05854655
Aswin Reji <aswinreji.004@gmail.com>
2024-12-12 23:50:20 +0000

    feat: Added a Eduction heading in about page need to complete this and need to do styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
+import About from "./pages/about/about";
 import Home from "./pages/home/home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
     <Navbar/>
     <Home/>
+    <About/>
     </>
   );
 }
---
+++ src/pages/about/about.tsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+    return (
+        <div id="about">
+            <h1>Education</h1>
+            <h4>Degree</h4>
+        </div>
+    )
+}

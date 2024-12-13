ee1ae64d50d35cee34b597bbfbd6adfe0b86286a
Aswin Reji <aswinreji.004@gmail.com>
2024-12-13 16:19:24 +0000

    feat: Added education detail need to clearly write more detailed explanation need to align prperly

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,9 +1,13 @@
+import './about.css'
 
 export default function About() {
     return (
         <div id="about">
             <h1>Education</h1>
-            <h4>Degree</h4>
+            <h4>Bachelor of Science Degree</h4>
+            <p>Kuriakose Gregorious College Kottayam</p>
+            <h4>Higher Secondary</h4>
+            <p>Ponkunnam Varkey Smaraka Higher Secondary School </p>
         </div>
     )
 }

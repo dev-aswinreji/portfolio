a6e79b66892d1ffb4478cee0c00e8ec63059706b
Aswin Reji <aswinreji.004@gmail.com>
2024-12-14 01:23:28 +0000

    feat: Need to add download resume button in about page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -2,24 +2,25 @@ import './about.css'
 
 export default function About() {
     return (
-<div className="about-container">
-    <h2 id='about' className="about-heading">About Me</h2>
-    <div className="education-container">
-        <div className="education-item">
-            <div className="education-item-content">
-                <h3>Bacherlor of Science in Physics</h3>
-                <p>Kuriakose Gregorious College,Kerala</p>
-                <p>India</p>
+        <div className="about-container">
+            {/* <a className='resume' href="./file.pdf" download="aswinreji-resume.pdf">Download</a> */}
+            <h2 id='about' className="about-heading">About Me</h2>
+            <div className="education-container">
+                <div className="education-item">
+                    <div className="education-item-content">
+                        <h3>Bacherlor of Science in Physics</h3>
+                        <p>Kuriakose Gregorious College,Kerala</p>
+                        <p>India</p>
+                    </div>
+                </div>
+                <div className="education-item">
+                    <div className="education-item-content">
+                        <h3>Higher Secondary (Science)</h3>
+                        <p>PVS Higher Secondary School,Kerala</p>
+                        <p>India</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="education-item">
-            <div className="education-item-content">
-                <h3>Higher Secondary (Science)</h3>
-                <p>PVS Higher Secondary School,Kerala</p>
-                <p>India</p>
-            </div>
-        </div>
-    </div>
-</div>
     )
 }

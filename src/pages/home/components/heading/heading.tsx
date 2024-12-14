ab4a85c19f67a61480f77c82976158456407db01
Aswin Reji <aswinreji.004@gmail.com>
2024-12-14 15:24:34 +0000

    feat: Almost done dark-mode thing need to add a good color and moving hand gestture after hey there

--- src/pages/home/components/heading/heading.tsx
+++ src/pages/home/components/heading/heading.tsx
@@ -3,7 +3,7 @@ export default function HomeHeading() {
         <div className="home">
             <div className="home-heading">
                 <h1>
-                    Hey There ,
+                    Hey There,
                 </h1>
                 <p>Myself ASWIN REJI an aspiring MERN STACK DEVELOPER</p>
                 <p>This is an overview of my skills and projects</p>

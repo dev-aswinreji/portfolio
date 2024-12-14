84a69a71c5e61e4200b447cd62f89b4298cfeff4
Aswin Reji <aswinreji.004@gmail.com>
2024-12-14 00:39:14 +0000

    fix: ui regarding project grid fixed

--- src/pages/home/components/heading/heading.tsx
+++ src/pages/home/components/heading/heading.tsx
@@ -1,11 +1,13 @@
 export default function HomeHeading() {
     return (
+        <div className="home">
         <div className="home-heading">
             <h1>
                 Welcome To My Portfolio 
             </h1>
             <p>Myself ASWIN REJI an aspiring MERN STACK DEVELOPER</p>
             <p>This is an overview of my skills and projects</p>
+        </div>
         </div>
     )
 }

4315a075e763fc82f38a7629ea4af60e3c646854
Aswin Reji <aswinreji.004@gmail.com>
2024-12-16 14:57:12 +0000

    fix: removed hss from about and added brocamp weeks

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -8,15 +8,15 @@ export default function About() {
             <div className="education-container">
                 <div className="education-item">
                     <div className="education-item-content">
-                        <h3>Bacherlor of Science in Physics</h3>
-                        <p>Kuriakose Gregorious College,Kerala</p>
+                        <h3>Web Development</h3>
+                        <p>Brototype, Banglore</p>
                         <p>India</p>
                     </div>
                 </div>
                 <div className="education-item">
                     <div className="education-item-content">
-                        <h3>Higher Secondary (Science)</h3>
-                        <p>PVS Higher Secondary School,Kerala</p>
+                        <h3>Bacherlor of Science in Physics</h3>
+                        <p>Kuriakose Gregorious College, Kerala</p>
                         <p>India</p>
                     </div>
                 </div>

57e12605e60e210d95fe50692f18907c7af48ae7
Aswin Reji <aswinreji.004@gmail.com>
2024-12-20 12:12:22 +0000

    feat: added mail icon and href

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,8 +1,14 @@
+import { MdMail } from "react-icons/md"
 
-export default function Contact(){
+export default function Contact() {
     return (
-        <div className="#contact">
-        <h5>email <a href="https://mailto:aswinreji.004@gmail.com">aswinreji.004@gmail.com</a></h5>
+
+        <div className="#contact" style={{ textAlign: "center" }}>
+            <h1>Contact Me</h1>
+            <span style={{display:"block"}}>
+                <MdMail />
+            </span>
+            <a target="_blank" style={{ textDecoration: "none" }} href="mailto:aswinreji.004@gmail.com">aswinreji.004@gmail.com</a>
         </div>
     )
 }

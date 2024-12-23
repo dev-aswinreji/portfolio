68cd0dbb09e3acdd0ab311e5ee257bb09add344a
Aswin Reji <aswinreji.004@gmail.com>
2024-12-23 18:25:11 +0000

    feat: Added phonenumber email added to .env for sercurity

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,18 +1,28 @@
-import { MdMail } from "react-icons/md"
+import { MdMail, MdPhone, MdLocationOn } from "react-icons/md";
+import "./contact.css";
 
 export default function Contact() {
     return (
-
-        <div className="#contact" style={{ textAlign: "center" }}>
-            <h1>Contact Me</h1>
-            <span style={{display:"block"}}>
-                <MdMail />
-            </span>
-            <a target="_blank" style={{ textDecoration: "none" }} href="mailto:aswinreji.004@gmail.com">aswinreji.004@gmail.com</a>
-            <span>
-                Instagram
-            </span>
-            <a href=""></a>
+        <div id="contact" className="contact-section">
+            <h1 className="contact-heading">Contact Me</h1>
+            <p className="contact-subheading">I'd love to hear from you!</p>
+            <div className="contact-info">
+                <div className="contact-item">
+                    <MdMail className="contact-icon" />
+                    <a href="" className="contact-link">
+                    </a>
+                </div>
+                <div className="contact-item">
+                    <MdPhone className="contact-icon" />
+                    <a href="tel:" className="contact-link">
+                        {import.meta.env.VITE_PHONENUMBER}
+                    </a>
+                </div>
+                {/* <div className="contact-item">
+                    <MdLocationOn className="contact-icon" />
+                    <span className="contact-text">City, Country</span>
+                </div> */}
+            </div>
         </div>
-    )
+    );
 }

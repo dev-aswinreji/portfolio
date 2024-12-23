a5fc6312545c085fa297109c56615753645c80ca
Aswin Reji <aswinreji.004@gmail.com>
2024-12-23 18:55:49 +0000

    feat: added github and linkedn link but linkedn got issue need to fix that

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,9 +1,12 @@
 import { MdMail, MdPhone, MdLocationOn } from "react-icons/md";
 import "./contact.css";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Contact() {
     const phonenumber = import.meta.env.VITE_PHONENUMBER
     const email = import.meta.env.VITE_EMAIL
+    const github = import.meta.env.VITE_GITHUB
+    const linkedn = import.meta.env.VITE_LINKEDN
     return (
         <div id="contact" className="contact-section">
             <h1 className="contact-heading">Contact Me</h1>
@@ -11,14 +14,24 @@ export default function Contact() {
             <div className="contact-info">
                 <div className="contact-item">
                     <MdMail className="contact-icon" />
-                    <a href={`mailto:${email}`} className="contact-link">
+                    <a href={`mailto:${email}`} target="_blank" className="contact-link">
                         {email}
                     </a>
                 </div>
                 <div className="contact-item">
                     <MdPhone className="contact-icon" />
-                    <a href={`tel:${phonenumber}`} className="contact-link">
+                    <a href={`tel:${phonenumber}`} target="_blank" className="contact-link">
                         {phonenumber}
+                    </a>
+                </div>
+                <div className="contact-item">
+                    <FaLinkedin className="contact-icon" />
+                    <a href={`${linkedn}`} target="_blank" className="contact-link">
+                        Linkedn
+                    </a>
+                    <FaGithub className="contact-icon" />
+                    <a href={`${github}`} target="_blank" className="contact-link">
+                        GitHub
                     </a>
                 </div>
                 {/* <div className="contact-item">
